c72af8857cb1fecc882264ea4fd22f219ddb4f12
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-25 15:52:42 -0500

    created tests for each case and all are passing

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -8,11 +8,31 @@ module PigLatin
       cut << 'yay'
       return cut
     elsif string.start_with?('a', 'e', 'i', 'o', 'u')
+      # string.match /^[aeiou](.*)/
+      # return "#{$1}way"
       cut = string.slice!(1, string.length)
       cut << 'way'
       return cut
     else
-      # if string[1] == ('a', 'e', 'i', 'o', 'u')
+      if /^[^aeiou][aeiouy](.*)/.match(string)
+        cut1 = string.slice(0)
+        cut2 = string[1] << $1
+        cut1 << 'ay'
+        return cut2 << cut1
+      elsif /^[^aeiou][^aeiouy][aeiouy](.*)/.match(string)
+        cut1 = string.slice(0..1)
+        cut2 = string[2] << $1
+        cut1 << 'ay'
+        return cut2 << cut1
+      else
+        cut1 = string.slice(0..2)
+        cut2 = string.slice(3..string.length)
+        cut1 << 'ay'
+        return cut2 << cut1
+      end
     end
   end
 end
+
+
+/[aeiou]/.match('elephant')
--- spec/pig_spec.rb
+++ spec/pig_spec.rb
@@ -19,5 +19,15 @@ describe PigLatin do
       result = pig_latin('love')
       expect(result).to eq 'ovelay'
     end
+
+    it 'works for consonant first & second letter' do
+      result = pig_latin('rhythm')
+      expect(result).to eq 'ythmrhay'
+    end
+
+    it 'works for consonant first/2nd/3rd' do
+      result = pig_latin('string')
+      expect(result).to eq 'ingstray'
+    end
   end
 end
