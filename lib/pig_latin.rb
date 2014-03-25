7a1da01f59a5da1763c7f9484e63e167bf41265b
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-25 14:54:12 -0500

    passed first test, if starts with Y it makes correct thing

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -3,6 +3,13 @@ require "pig_latin/version"
 module PigLatin
   # Your code goes here...
   def pig_latin(string)
+    if string.start_with?('y')
+      cut = string.slice!(1, string.length)
+      cut << 'yay'
+      return cut
+    elsif string.start_with?('a', 'e', 'i', 'o', 'u')
 
+
+    end
   end
 end
