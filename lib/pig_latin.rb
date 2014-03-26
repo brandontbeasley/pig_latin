abfcc5668b0f2c491c79a8887824c785c39905b5
vagrant <vagrant@student-dev-box>
2014-03-26 14:07:57 +0000

    refactored pig latin gem

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -2,34 +2,39 @@ require "pig_latin/version"
 
 module PigLatin
   # Your code goes here...
+
   def pig_latin(string)
     if string.start_with?('y')
       cut = string.slice!(1, string.length)
       cut << 'yay'
       return cut
     elsif string.start_with?('a', 'e', 'i', 'o', 'u')
-      # string.match /^[aeiou](.*)/
-      # return "#{$1}way"
       cut = string.slice!(1, string.length)
       cut << 'way'
       return cut
-    else
-      if /^[^aeiou][aeiouy](.*)/.match(string)
-        cut1 = string.slice(0)
-        cut2 = string[1] << $1
-        cut1 << 'ay'
-        return cut2 << cut1
-      elsif /^[^aeiou][^aeiouy][aeiouy](.*)/.match(string)
-        cut1 = string.slice(0..1)
-        cut2 = string[2] << $1
-        cut1 << 'ay'
-        return cut2 << cut1
-      else
-        cut1 = string.slice(0..2)
-        cut2 = string.slice(3..string.length)
-        cut1 << 'ay'
-        return cut2 << cut1
-      end
+    elsif /^([^aeiou]+)([aeiouy].*)/.match(string)
+      last = $1 << 'ay'
+      full = $2 << last
+      return full
+    end
+
+      # if /^([^aeiou]+[aeiouy])(.*)/.match(string)
+
+      #   cut1 = string.slice(0)
+      #   cut2 = string[1] << $1
+      #   cut1 << 'ay'
+      #   return cut2 << cut1
+      # elsif /^[^aeiou][^aeiouy][aeiouy](.*)/.match(string)
+      #   cut1 = string.slice(0..1)
+      #   cut2 = string[2] << $1
+      #   cut1 << 'ay'
+      #   return cut2 << cut1
+      # else
+      #   cut1 = string.slice(0..2)
+      #   cut2 = string.slice(3..string.length)
+      #   cut1 << 'ay'
+      #   return cut2 << cut1
+      # end
     end
   end
 end
